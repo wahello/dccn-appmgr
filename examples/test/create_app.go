e0feaff6cfae338342987058d8417dddd0b6dd20
benforest <hanping@ankr.network>
2019-04-11 18:52:51 -0700

    SWDEV-184 update AppStatus within AppReport

---
+++ examples/test/create_app.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	//	"github.com/Ankr-network/dccn-common/protos"
+
+	"log"
+	"time"
+
+	appmgr "github.com/Ankr-network/dccn-common/protos/appmgr/v1/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+
+	//usermgr "github.com/Ankr-network/dccn-common/protos/usermgr/v1/grpc"
+	common_proto "github.com/Ankr-network/dccn-common/protos/common"
+	//	apiCommon "github.com/Ankr-network/dccn-hub/app-dccn-api/examples/common"
+)
+
+var addr = "appmgr:50051"
+
+func main() {
+
+	log.SetFlags(log.LstdFlags | log.Llongfile)
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer func(conn *grpc.ClientConn) {
+		if err := conn.Close(); err != nil {
+			log.Println(err.Error())
+		}
+	}(conn)
+
+	appClient := appmgr.NewAppMgrClient(conn)
+
+	md := metadata.New(map[string]string{
+		"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NTUwMTUzMTgsImp0aSI6IjQ4NTQ5YjQxLWUzNjYtNGIxMi05NTc3LTU0M2Y5NTE5Y2JlZiIsImlzcyI6ImFua3IubmV0d29yayJ9.A0p3KyxIKZHAZb_buPgadKj3d40Rlw_hSpsFBrNLjuw",
+	})
+
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	//
+	tokenContext, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	//
+	app := common_proto.App{}
+	app.Name = "workpress_test"
+	app.ChartDetail = &common_proto.ChartDetail{
+		Repo:    "stable",
+		Name:    "workpress",
+		Version: "5.7.1",
+	}
+	app.NamespaceData = &common_proto.App_Namespace{
+		Namespace: &common_proto.Namespace{
+			Name:         "test_ns1",
+			CpuLimit:     300,
+			MemLimit:     500,
+			StorageLimit: 10,
+		},
+	}
+
+	app.Attributes = &common_proto.AppAttributes{}
+
+	if rsp, err := appClient.CreateApp(tokenContext, &appmgr.CreateAppRequest{App: &app}); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("create app successfully : appid   " + rsp.AppId)
+	}
+
+}
